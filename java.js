9ac046a9baeff8209618f604ef375443505d9272
emmyjr <magbroko@gmail.com>
2020-09-24 22:32:45 +0100

    FirstPush

---
+++ java.js
@@ -0,0 +1,11 @@
+function myfunction() {
+    document.getElementById("demo").innerHTML = "Paragraph Change"
+}
+function myFunction() {
+    document.getElementById("dem21").style.fontSize = "30px";
+}
+document.getElementById("red1").innerHTML = "John Joe";
+function toCelsius(f) {
+    return (5/9) * (f-32)
+}
+document.getElementById("sea").innerHTML = toCelsius(77);
